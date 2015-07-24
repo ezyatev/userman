541afaee0b6f704b4f49e96492200733ba1aa2fc
eu <eu@f1dev.com>
2015-07-24 17:20:05 +0300

    Code refactored.

--- userman.py
+++ userman.py
@@ -1,6 +1,6 @@
 r"""userman - simple web server management tool.
 
-This script creates unix account, apache virtual host config for the specified domain name and MySQL database.
+This script creates UNIX account, apache virtual host config for the specified domain name and MySQL database.
 
 Principles are:
     1. One domain per user.
@@ -20,33 +20,13 @@ import uuid
 import sys
 import crypt
 
+USER_HOME = '/home/%(user)s/'
+DOMAIN_HOME = '%(domain)s/public'
+SFTP_USER_GROUP = 'sftp'
+PASSWORD_SALT = 'en2llow9w'
 
-def get_salt():
-    return "en2llow9w"
-
-
-def get_sftp_group():
-    return "sftp"
-
-
-def get_apache_host_file(domain):
-    return '/etc/apache2/sites-available/%s.conf' % domain
-
-
-def get_user_home(kwargs):
-    return '/home/%(user)s/' % kwargs
-
-
-def get_domain_home(kwargs):
-    return '%(domain)s/public' % kwargs
-
-
-def get_random_password():
-    return call_command('mkpasswd %u' % uuid.uuid4())
-
-
-def get_apache_template(kwargs):
-    return """
+APACHE_HOST_FILE = '/etc/apache2/sites-available/%(domain)s.conf'
+APACHE_HOST_CONTENT = """
 <VirtualHost *:80>
     ServerName %(domain)s
     ServerAlias www.%(domain)s
@@ -64,23 +44,19 @@ def get_apache_template(kwargs):
     ErrorLog ${APACHE_LOG_DIR}/%(domain)s.error.log
     CustomLog ${APACHE_LOG_DIR}/%(domain)s.access.log combined
 </VirtualHost>
-    """ % kwargs
+"""
 
-
-def get_sql_query():
-    return """
+SQL_QUERY = """
 CREATE USER %(user)s;
 CREATE DATABASE IF NOT EXISTS %(db)s;
 GRANT ALL PRIVILEGES ON %(db)s.* TO '%(user)s'@'localhost' IDENTIFIED BY '%(password)s';
 FLUSH PRIVILEGES;
 """
 
-
-def get_report():
-    return """
------------------------
-Completed successfully.
------------------------
+CREDENTIALS = """
+-------------------------------------------
+%(domain)s credentials:
+-------------------------------------------
 SFTP:
     Host: %(domain)s
     User: %(user)s
@@ -93,13 +69,17 @@ MySQL:
 """
 
 
-def write_apache_host_file(domain, content):
-    f = open(get_apache_host_file(domain), 'w')
+def random_password():
+    return call('mkpasswd %u' % uuid.uuid4())
+
+
+def write_apache_host(domain, content):
+    f = open(APACHE_HOST_FILE % dict(domain=domain), 'w')
     f.write(content)
     f.close()
 
 
-def call_command(command):
+def call(command):
     output = None
     try:
         output = subprocess.check_output(command, shell=True)
@@ -110,62 +90,59 @@ def call_command(command):
 
 
 def main():
-    unix_password = get_random_password()
-    mysql_password = get_random_password()
+    unix_password, mysql_password = random_password(), random_password()
 
     # create user
     user, domain = raw_input('Username: '), raw_input('Site domain: ')
-    call_command(
+    call(
         'useradd -p %(password)s %(user)s' %
         dict(
             user=user,
-            password=crypt.crypt(unix_password, get_salt())
+            password=crypt.crypt(unix_password, PASSWORD_SALT)
         )
     )
 
     # user home create
-    user_home = get_user_home(dict(user=user))
-    call_command('sudo mkdir -p %(user_home)s' % dict(user_home=user_home))
-    call_command('chown %(user)s:%(user)s %(user_home)s' % dict(user=user, user_home=user_home))
+    user_home = USER_HOME % dict(user=user)
+    call('sudo mkdir -p %(user_home)s' % dict(user_home=user_home))
+    call('chown %(user)s:%(user)s %(user_home)s' % dict(user=user, user_home=user_home))
 
     # domain home create
     print('Creating domain home directory...')
-    domain_home = user_home + get_domain_home(dict(domain=domain))
-    call_command('sudo -u %(user)s mkdir -p %(domain_home)s' % dict(user=user, domain_home=domain_home))
+    domain_home = user_home + DOMAIN_HOME % dict(domain=domain)
+    call('sudo -u %(user)s mkdir -p %(domain_home)s' % dict(user=user, domain_home=domain_home))
 
     # change owner of the user home to root:root
-    call_command('chown root:root %(user_home)s' % dict(user_home=user_home))
+    call('chown root:root %(user_home)s' % dict(user_home=user_home))
 
     # write apache host config
     print('Apache host config writing...')
-    apache_host_content = get_apache_template(
-        dict(
-            domain_home=domain_home,
-            domain=domain,
-            user=user
-        )
+    apache_host_content = APACHE_HOST_CONTENT % dict(
+        domain_home=domain_home,
+        domain=domain,
+        user=user
     )
-    write_apache_host_file(domain, apache_host_content)
+    write_apache_host(domain, apache_host_content)
 
     # enable new host
     print('New apache host enable...')
-    call_command('a2ensite %(domain)s' % dict(domain=domain))
+    call('a2ensite %(domain)s' % dict(domain=domain))
 
     # add user to sftp group
     print('Adding user to sftp group...')
-    call_command('usermod -a -G %(group)s %(user)s' % dict(user=user, group=get_sftp_group()))
+    call('usermod -a -G %(group)s %(user)s' % dict(user=user, group=SFTP_USER_GROUP))
 
     # create mysql database
     print('Creating mysql user, database and grant permissions...')
-    sql = get_sql_query() % dict(
+    sql = SQL_QUERY % dict(
         user=user,
         db=user,
         password=mysql_password
     )
-    call_command('mysql -uroot -p -e "%(sql)s"' % dict(sql=sql))
+    call('mysql -uroot -p -e "%(sql)s"' % dict(sql=sql))
 
     # report
-    report = get_report() % dict(
+    report = CREDENTIALS % dict(
         domain=domain,
         user=user,
         unix_password=unix_password,
