4a9543f724d559f1c8bf4055ef09fd263fffcf06
eu <eu@f1dev.com>
2015-07-25 17:49:42 +0300

    New script version initial commit.

--- userman.py
+++ userman.py
@@ -6,150 +6,111 @@ Principles are:
     1. One domain per user.
     2. User can only log in via sftp and only in the own home directory. It is necessary to create the sftp user group.
     3. Separated database for the user.
-
-Possible sshd_config file settings:
-    Subsystem sftp internal-sftp
-    Match Group sftp
         ChrootDirectory %h
         ForceCommand internal-sftp
         AllowTcpForwarding no
 """
 
-import subprocess
 import uuid
 import sys
 import crypt
+import textwrap
 
-USER_HOME = '/home/%(user)s/'
-DOMAIN_HOME = '%(domain)s/public'
-SFTP_USER_GROUP = 'sftp'
-PASSWORD_SALT = 'en2llow9w'
-
-APACHE_HOST_FILE = '/etc/apache2/sites-available/%(domain)s.conf'
-APACHE_HOST_CONTENT = """
-<VirtualHost *:80>
-    ServerName %(domain)s
-    ServerAlias www.%(domain)s
-    ServerAdmin webmaster@%(domain)s
-
-    AssignUserId %(user)s %(user)s
-
-    DocumentRoot "%(domain_home)s"
-
-    <Directory "%(domain_home)s">
-        AllowOverride all
-        Require all granted
-    </Directory>
-
-    ErrorLog ${APACHE_LOG_DIR}/%(domain)s.error.log
-    CustomLog ${APACHE_LOG_DIR}/%(domain)s.access.log combined
-</VirtualHost>
-"""
-
-SQL_QUERY = """
-CREATE USER %(user)s;
-CREATE DATABASE IF NOT EXISTS %(db)s;
-GRANT ALL PRIVILEGES ON %(db)s.* TO '%(user)s'@'localhost' IDENTIFIED BY '%(password)s';
-FLUSH PRIVILEGES;
-"""
-
-CREDENTIALS = """
--------------------------------------------
-%(domain)s credentials:
--------------------------------------------
-SFTP:
-    Host: %(domain)s
-    User: %(user)s
-    Password: %(unix_password)s
-
-MySQL:
-    Host: localhost
-    User: %(user)s
-    Password: %(mysql_password)s
-"""
+import settings
+from validator import User, DomainName
 
 
-def random_password():
-    return call('mkpasswd %u' % uuid.uuid4())
+class Base(object):
+    def __init__(self):
+        self.__user = None
+        self.__domain = None
+
+    @property
+    def user(self):
+        if not self.__user:
+            string = raw_input('User: ')
+            if User.validate(string):
+                self.__user = string
+        return self.__user
+
+    @property
+    def domain(self):
+        if not self.__domain:
+            string = raw_input('Domain: ')
+            if DomainName.validate(string):
+                self.__domain = string
+        return self.__domain
+
+    def random_password(self):
+        return self.call('mkpasswd %u' % uuid.uuid4())
+
+    def call(self, command):
+        output = None
+        try:
+            output = command
+            # output = subprocess.check_output(command, shell=True)
+        except Exception as e:
+            print("An error occurred while processing '%s' command: %s." % (command, e))
+            sys.exit()
+        return output.strip()
+
+    def process(self):
+        return NotImplementedError('You should to implement the process method.')
+
+    def report(self, message, **kwargs):
+        print(textwrap.dedent(message) % kwargs)
 
 
-def write_apache_host(domain, content):
-    f = open(APACHE_HOST_FILE % dict(domain=domain), 'w')
-    f.write(content)
-    f.close()
+class SystemUser(Base):
+    @property
+    def __unix_password(self):
+        return crypt.crypt(self.random_password(), settings.PASSWORD_SALT)
+
+    def create(self):
+        kwargs = dict(
+            user=self.user,
+            password=self.__unix_password,
+            user_home=settings.USER_HOME % dict(user=self.user)
+        )
+        self.call(
+            'useradd -p %(password)s %(user)s -d %(user_home)s' % kwargs
+        )
+        message = (
+            """
+            System user was successfully created.
+            --------------------------------------------
+            User: %(user)s
+            Password: %(password)s
+            Home dir: %(user_home)s
+            --------------------------------------------
+            """
+        )
+        self.report(message % kwargs)
+
+    def add_to_group(self):
+        kwargs = dict(user=self.user, group=settings.SFTP_GROUP)
+        self.call('usermod -a -G %(group)s %(user)s' % kwargs)
+        self.report('User %(user)s successfully added to the %(group)s group.' % kwargs)
+
+    def process(self):
+        self.create()
+        self.add_to_group()
 
 
-def call(command):
-    output = None
-    try:
-        output = subprocess.check_output(command, shell=True)
-    except Exception as e:
-        print("An error occurred while processing '%s' command: %s." % (command, e))
-        sys.exit()
-    return output.strip()
+class ApacheHost(Base):
+    def __init__(self):
+        super(ApacheHost, self).__init__()
+        print(self.domain)
+
+
+class MySQLDatabase(Base):
+    pass
 
 
 def main():
-    unix_password, mysql_password = random_password(), random_password()
-
-    # create user
-    user, domain = raw_input('Username: '), raw_input('Site domain: ')
-    call(
-        'useradd -p %(password)s %(user)s' %
-        dict(
-            user=user,
-            password=crypt.crypt(unix_password, PASSWORD_SALT)
-        )
-    )
-
-    # user home create
-    user_home = USER_HOME % dict(user=user)
-    call('sudo mkdir -p %(user_home)s' % dict(user_home=user_home))
-    call('chown %(user)s:%(user)s %(user_home)s' % dict(user=user, user_home=user_home))
-
-    # domain home create
-    print('Creating domain home directory...')
-    domain_home = user_home + DOMAIN_HOME % dict(domain=domain)
-    call('sudo -u %(user)s mkdir -p %(domain_home)s' % dict(user=user, domain_home=domain_home))
-
-    # change owner of the user home to root:root
-    call('chown root:root %(user_home)s' % dict(user_home=user_home))
-
-    # write apache host config
-    print('Apache host config writing...')
-    apache_host_content = APACHE_HOST_CONTENT % dict(
-        domain_home=domain_home,
-        domain=domain,
-        user=user
-    )
-    write_apache_host(domain, apache_host_content)
-
-    # enable new host
-    print('New apache host enable...')
-    call('a2ensite %(domain)s' % dict(domain=domain))
-
-    # add user to sftp group
-    print('Adding user to sftp group...')
-    call('usermod -a -G %(group)s %(user)s' % dict(user=user, group=SFTP_USER_GROUP))
-
-    # create mysql database
-    print('Creating mysql user, database and grant permissions...')
-    sql = SQL_QUERY % dict(
-        user=user,
-        db=user,
-        password=mysql_password
-    )
-    call('mysql -uroot -p -e "%(sql)s"' % dict(sql=sql))
-
-    # report
-    report = CREDENTIALS % dict(
-        domain=domain,
-        user=user,
-        unix_password=unix_password,
-        mysql_password=mysql_password
-    )
-
-    print(report)
+    for entity in Base.__subclasses__():
+        e = entity()
+        e.process()
 
 
 if __name__ == '__main__':
---
+++ validator.py
@@ -0,0 +1,28 @@
+import re
+import sys
+
+sys.tracebacklimit = 0
+
+
+class Validator(object):
+    @staticmethod
+    def validate(string):
+        return NotImplementedError('You should to implement the validate() method.')
+
+
+class User(Validator):
+    @staticmethod
+    def validate(string):
+        match = re.match('[a-z_]+', string)
+        if match:
+            return True
+        raise Exception('Incorrect Unix username.')
+
+
+class DomainName(Validator):
+    @staticmethod
+    def validate(string):
+        match = re.match('^([a-z0-9]+\.)*[a-z0-9]+\.[a-z]+$', string)
+        if match:
+            return True
+        raise Exception('Incorrect domain name.')
